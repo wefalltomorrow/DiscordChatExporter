68c8467677d64d304a2a90cf603d429d3929eca5
wefalltomorrow <45746576+wefalltomorrow@users.noreply.github.com>
2026-09-19 00:56:25 +1000

    Add missing CLI cancellation namespace

--- DiscordChatExporter.Cli/Commands/Base/ExportCommandBase.cs
+++ DiscordChatExporter.Cli/Commands/Base/ExportCommandBase.cs
@@ -5,6 +5,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using CliFx;
 using CliFx.Binding;
