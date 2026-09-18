c411a07fbd73df7d00cc0baf524b367a35460cfc
wefalltomorrow <45746576+wefalltomorrow@users.noreply.github.com>
2026-09-19 01:19:20 +1000

    Wire Russian localization with English fallback

--- DiscordChatExporter.Gui/Localization/LocalizationManager.cs
+++ DiscordChatExporter.Gui/Localization/LocalizationManager.cs
@@ -44,12 +44,14 @@ public partial class LocalizationManager : ObservableObject, IDisposable
                     "deu" => GermanLocalization,
                     "fra" => FrenchLocalization,
                     "spa" => SpanishLocalization,
+                    "rus" => RussianLocalization,
                     _ => EnglishLocalization,
                 },
             Language.Ukrainian => UkrainianLocalization,
             Language.German => GermanLocalization,
             Language.French => FrenchLocalization,
             Language.Spanish => SpanishLocalization,
+            Language.Russian => RussianLocalization,
             _ => EnglishLocalization,
         };
 
